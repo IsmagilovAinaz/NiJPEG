4aa11e62e05bc109552014d420dcd5ba78e5ffcd
Rush-IT <78534165+Rush-IT@users.noreply.github.com>
2024-01-17 12:08:29 +0500

    Add logger

--- MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -15,15 +17,43 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class MainActivity extends AppCompatActivity {
 
+    final String LOG_FILE_NAME =  "AppLog%g.log";
+    final int LOG_FILE_C = 5;
+    final int LOG_SIZE = 102400;
+    final boolean LOG_APPEND = true;
+    Logger logger;
     int CODE_HTTP = 0;
+    int REQUEST_OK = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logger = Logger.getLogger(MainActivity.class.getName());
+        logger.log(Level.INFO, "application start");
+
+        try {
+            String logName = Environment.getExternalStorageDirectory() +
+                    File.separator +
+                    LOG_FILE_NAME;
+            FileHandler logHandler = new FileHandler(logName, LOG_SIZE, LOG_FILE_C, LOG_APPEND);
+            logHandler.setFormatter(new SimpleFormatter());
+            logger.addHandler(logHandler);
+            logger.log(Level.INFO, "Logger is ready");
+        }
+        catch (Exception e) {
+            logger.log(Level.SEVERE, "Logger is not ready: unexpected", e);
+        }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         else if(id == R.id.itemRecon){
             RequestSender requestSender = new RequestSender();
             requestSender.execute();
-            if(CODE_HTTP==200){
+            if(CODE_HTTP==REQUEST_OK){
                 //Online
                 ImageView imageView = findViewById(R.id.imageView);
                 imageView.setImageBitmap(ConnectionHttp.getImage());
@@ -60,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             }
             else{
                 //Offline
+                logger.log(Level.SEVERE, String.format("connection error: code %d",CODE_HTTP));
                 TextView textView = findViewById(R.id.statusId);
                 textView.setText("Offline");
                 textView.setTextColor(Color.parseColor("#FF0000"));
@@ -77,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
             try {
                 CODE_HTTP = ConnectionHttp.start();
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                logger.log(Level.SEVERE, "timeout: unexpected");
             }
             return null;
         }
